ac4877263e08f209d74c305168db6f399f659b7a
Ramadan <daudalaramadan2@gmail.com>
2024-04-10 22:13:45 +0000

    styling done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="wrapper">
+      <div className="content-wrapper">
+        <div className="header">My Todo App</div>
+        <form>
+          <label className="i1">
+            <input className="gap" type="text" name="name" />
+            <input type="submit" value="Submit" />
+          </label>
+          <label className="i2">
+            <input className="gap" type="text" name="name" />
+            <input type="submit" value="Submit" />
+          </label>
+          <label className="i3">
+            <input className="gap" type="text" name="name" />
+            <input type="submit" value="Submit" />
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
