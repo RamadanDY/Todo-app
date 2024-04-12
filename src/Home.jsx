4818ffc245feef0494d6d715c37d37c490a5b1e1
Ramadan <daudalaramadan2@gmail.com>
2024-04-12 01:00:21 +0000

    installing packages

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import './App.css'
 import Create from './Create'
+import { useState } from 'react'
 
 const Home = () => {
+    // 1.newstate created
+    const [todos, setTodos] = useState([]);
   return (
     <div>
         <div className="wrapper">
             <div className="content-wrapper">
                 <div className="header">My Todo App</div>
                 <Create />
+                {/*  */}
+
+                {
+                    todos.length === 0 ? 
+                    <div><h2>no record</h2></div>
+                    :
+                
+                    todos.map(todo => {
+                        <div>
+                            {todo}
+                        </div>
+                    })
+                }
             </div>
         </div>
     </div>
