286ad7466a8aa38ece00ad42e0813ae2f52d2ed4
Ramadan <daudalaramadan2@gmail.com>
2024-04-15 22:13:43 +0000

    axios

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 const Create = () => {
+  
+  const [task, setTask] = useState('');
+
+  // Function to handle adding task
+  const handleAdd = (event) => {
+    event.preventDefault(); // Prevent form submission
+    axios.post('http://localhost:3001/add', { task: task })
+      .then(result => console.log(result))
+      .catch()
+  
+  }
+
   return (
     <div>
         <form>
             <label className="i1">
-               <input className="gap" name="name" type="text" />
-               <button type="submit">Add</button>
+               <input className="gap" name="name" type="text" onChange={(event) => setTask(event.target.value)} />
+               <button type="button" onClick={handleAdd}>Add</button> {/* Change type to "button" */}
             </label>
         </form>
     </div>
   )
 }
 
-export default Create
+export default Create;
