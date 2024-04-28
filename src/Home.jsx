f851d44290368a5b619aeadcb18e92bc0928fd55
Ramadan <daudalaramadan2@gmail.com>
2024-04-28 16:00:11 +0000

    making the text visible

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,48 +1,50 @@
 import React from 'react'
 import './App.css'
 import Create from './Create'
-import { useState } from 'react'
+import axios from 'axios'
+import { useState ,useEffect } from 'react'
 
 const Home = () => {
     // to store and display out text using the map method
     const [todos, setTodos] = useState([]);
+    useEffect(() =>{
+        // we will use axios to fetch the data
+        // always remmber to specify your route and create the get api somewhere
+        // in this case its our index.js  
+        axios.get('http://localhost:8080/get')
+        // this case we want the data that was updated inside the settodo
+        // we will store our result or data inside our setTodo
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err))
+
+    },[])
+
   return (
     <div>
         <div className="wrapper">
             <div className="content-wrapper">
                 <div className="header">My Todo App</div>
                 <Create />
-                {/*  */}
-
-                {/* if (todos.length === 0 ){
-                    <div>
-                        <h2>no record</h2>
-                    </div>
-
-                }else {
-                    todos.map((todo) =>{
-                        <div>
-                            {todo}
-                        </div>
-
-                    })
-                } */}
-
+               
                 {
 
 
                     todos.length === 0 ? 
                     <div><h2>no records</h2></div>
+                    
                     :
                     // The map method is a way to loop over each item in an array and perform some
                     //  operation on each item.
                     // right now because of the {todo} ,its empty 
                     // so above us is a condition
                     todos.map(todo => {
-                        <div>
-                            {todo}
-                        </div>
+                        return (
+                            <div key={todo.id}>
+                                {todo.task}
+                            </div>
+                        );
                     })
+                    
                 }
             </div>
         </div>
